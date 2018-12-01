09fed9f69272e4d7e522f296c46f03e4083de124
David <dshen1@bu.edu>
2018-12-01 11:58:27 -0500

    Server now returns a proper http response

--- webserv.c
+++ webserv.c
@@ -7,12 +7,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <signal.h>
 
-// all this does is send a message to the client right now
-int handle_request(int client) {
-    // write to the client_socket
-    char message[] = "You've connected to the server";
-    return send(client, message, strlen(message), 0);
+enum {
+    BUF_SIZE = 256  /* size to hold http requests and responses */
+};
+
+// all this does right now is send back the client's request
+int handle_request(int client, char* request, ssize_t request_size) {
+    // must send a correctly formatted HTTP response or else the browser wont display
+    char response[BUF_SIZE];
+    snprintf(response, BUF_SIZE, "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %li\r\n\r\n", request_size);
+    if (send(client, response, strlen(response), 0) == -1) {
+        perror("Send error");
+        return -1;
+    }
+    if (send(client, request, request_size, 0) == -1) {
+        perror("Send error");
+        return -1;
+    }
+    return 0;
 }
 
 // code adapted from APUE textbook (pg 615)
@@ -28,21 +42,21 @@ int serve(int server) {
         }
 
         // process the client's request by forking a child process
+        // I think forking causes the connection to the client to occur twice
         if ((pid = fork()) < 0) {
             perror("fork error");
         } else if (pid == 0) {
-            printf("Child process running request for client %d\n", client_fd);
-            int ret = handle_request(client_fd);
-            printf("request handled with return %d\n", ret);
+            char buf[BUF_SIZE];
+            ssize_t n_read = recv(client_fd, buf, BUF_SIZE, 0);
+            int ret = handle_request(client_fd, buf, n_read);
+            printf("Request handled with return %d\n", ret);
             close(client_fd);
             exit(0); // terminate the child process
         } else {
             /* parent */
-            printf("Parent process running\n");
             close(client_fd); // close the client fd because the child still maintains a copy
             waitpid(pid, &status, 0);
         }
-        printf("Exiting fork\n");
     }
 }
 
@@ -62,7 +76,11 @@ int start_server(uint16_t port) {
     if (bind(webserver, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
         perror("Bind error");
     }
-    listen(webserver, 5); // have our webserver listen to up to 5 connections
+    
+    // have our webserver listen to up to 5 connections
+    if (listen(webserver, 5) == -1) {
+        perror("Listen error");
+    } 
     
     serve(webserver);
 
@@ -71,14 +89,15 @@ int start_server(uint16_t port) {
     return 0;
 }
 
+
 int main(int argc, char const *argv[]) {
     if (argc != 2) {
         printf("Error: to start server use: $./webserv port-number\n");
         exit(1);
     }
-
     uint16_t port = (uint16_t) strtol(argv[1], NULL, 10);
     printf("Starting server on port %u\n", port);
+
     if (start_server(port) != 0) {
         perror("Server Error");
         exit(1);
