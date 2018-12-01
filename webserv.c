28243ea17adc3a9d2da34b7c598c23fbde792580
David <dshen1@bu.edu>
2018-12-01 02:20:17 -0500

    Allowed webserver to keep accepting connections

--- webserv.c
+++ webserv.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -12,6 +13,37 @@ int handle_request(int client) {
     // write to the client_socket
     char message[] = "You've connected to the server";
     return send(client, message, strlen(message), 0);
+}
+
+// code adapted from APUE textbook (pg 615)
+int serve(int server) {
+    int client_fd, status;
+    pid_t pid;
+    for(;;) {
+        // accept a new connection (from our local machine)
+        // accept will block if no connections are pending
+        if ((client_fd = accept(server, NULL, NULL)) < 0) {
+            perror("Accept error");
+            exit(1);
+        }
+
+        // process the client's request by forking a child process
+        if ((pid = fork()) < 0) {
+            perror("fork error");
+        } else if (pid == 0) {
+            printf("Child process running request for client %d\n", client_fd);
+            int ret = handle_request(client_fd);
+            printf("request handled with return %d\n", ret);
+            close(client_fd);
+            exit(0); // terminate the child process
+        } else {
+            /* parent */
+            printf("Parent process running\n");
+            close(client_fd); // close the client fd because the child still maintains a copy
+            waitpid(pid, &status, 0);
+        }
+        printf("Exiting fork\n");
+    }
 }
 
 int start_server(uint16_t port) {
@@ -32,15 +64,7 @@ int start_server(uint16_t port) {
     }
     listen(webserver, 5); // have our webserver listen to up to 5 connections
     
-    // accept a new connection (from our local machine)
-    int client_socket = accept(webserver, NULL, NULL);
-    printf("Connection accepted for client %d\n", client_socket);
-
-    // process the client's request by forking a child process
-    if (fork() == 0) {
-        printf("Child process running request for client %d\n", client_socket);
-        handle_request(client_socket);
-    }
+    serve(webserver);
 
     // might want to set up a signal handler for sigint to call close
     close(webserver);
@@ -56,7 +80,7 @@ int main(int argc, char const *argv[]) {
     uint16_t port = (uint16_t) strtol(argv[1], NULL, 10);
     printf("Starting server on port %u\n", port);
     if (start_server(port) != 0) {
-        perror("Error: ");
+        perror("Server Error");
         exit(1);
     }
     return 0;
