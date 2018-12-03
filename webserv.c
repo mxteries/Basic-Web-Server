1ace303d8d6dd200d7bb173fc479f2f805d16254
David <dshen1@bu.edu>
2018-12-02 22:34:51 -0500

    Removed content-length header and started handling request file

--- webserv.c
+++ webserv.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -42,7 +44,7 @@ void get_date(char* buf, int date_size) {
  * len: content length (ie. size of body)
  * body: the content being requested
  */  
-void create_response(char* buf, int status, char* type, size_t len, char* body) {
+void create_response(char* buf, int status, char* type, char* body) {
     char date[BUF_SIZE];
     get_date(date, BUF_SIZE);
 
@@ -58,8 +60,8 @@ void create_response(char* buf, int status, char* type, size_t len, char* body) 
         strcpy(status_msg, "Unknown");
     }
 
-    char* response = "HTTP/1.1 %d %s\r\nDate: %s\r\nConnection: close\r\nContent-Type: %s\r\nContent-Length: %lu\r\n\r\n%s\n\n";
-    snprintf(buf, BUF_SIZE, response, status, status_msg, date, type, len, body);
+    char* response = "HTTP/1.1 %d %s\r\nDate: %s\r\nConnection: close\r\nContent-Type: %s\r\n\r\n%s\n\n";
+    snprintf(buf, BUF_SIZE, response, status, status_msg, date, type, body);
 }
 
 // log a response, and send it
@@ -74,7 +76,7 @@ int send_response(int client, char* response) {
 void send_404(int client) {
     char* err_msg = "The requested item could not be found";
     char response[BUF_SIZE];
-    create_response(response, 404, "text/plain", strlen(err_msg), err_msg);
+    create_response(response, 404, "text/plain", err_msg);
     if (send_response(client, response) == -1) {
         perror("404 send error");
     }
@@ -84,22 +86,45 @@ void send_404(int client) {
 void send_501(int client) {
     char* err_msg = "The requested method has not implemented";
     char response[BUF_SIZE];
-    create_response(response, 501, "text/plain", strlen(err_msg), err_msg);
+    create_response(response, 501, "text/plain", err_msg);
     if (send_response(client, response) == -1) {
         perror("501 send error");
     }
 }
 
-// all this does right now is send back the client's request
-// TODO: determine different content-types
+// code adapted from https://stackoverflow.com/a/5309508
+char get_file_extension(char* filename) {
+    char *dot = strrchr(filename, '.');
+    if (dot == NULL || dot == filename) 
+        return 0;
+    return *(dot + 1);
+}
+
+// if file exists, it is either a dir. file or a reg. file, else send 404
+// if dir. file, call ls on it
+// if reg file, determine file extension (eg. .html, .py, .jpg, etc.)
 void handle_GET(int client, char* requested_file) {
     // first determine if file exists
-
-    char response[BUF_SIZE];
-    create_response(response, 200, "unknown", strlen(requested_file), requested_file);
-    if (send_response(client, response) == -1) {
-        perror("GET response send error");
+    struct stat sb;
+    if (stat(requested_file, &sb) == -1) {
+        /* if stat fails, assume file doesn't exist */
+        perror("stat");
+        send_404(client);
+    } else if (S_ISDIR(sb.st_mode)) {
+        send_501(client);
+    } else if (S_ISREG(sb.st_mode)) {
+        char response[BUF_SIZE];
+        create_response(response, 200, "unknown", requested_file);
+        if (send_response(client, response) == -1) {
+            perror("GET response send error");
+        }
+    } else {
+        /* unknown file type */
+        send_501(client);
     }
+
+    // if its a regular file, it is either a .html, .jpg/.jpeg/.gif, a .cgi, or a .py file
+    // it it's not one of those, return a 501 error
 }
 
 // determines the method of the request (eg. GET) and the requested file
@@ -123,7 +148,7 @@ void handle_request(int client, char* request, ssize_t request_size) {
     }
 }
 
-// code adapted from APUE textbook (pg 615)
+// code adapted from APUE textbook (pg 617-618)
 int serve(int server) {
     int client_fd, status;
     pid_t pid;
@@ -138,6 +163,7 @@ int serve(int server) {
         // process the client's request by forking a child process
         if ((pid = fork()) < 0) {
             perror("fork error");
+            exit(1);
         } else if (pid == 0) {
             char buf[BUF_SIZE];
             ssize_t n_read = recv(client_fd, buf, BUF_SIZE, 0);
@@ -163,8 +189,7 @@ int start_server(uint16_t port) {
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    // we defined an address for our webserver, 
-    // now let's connect them together using bind
+    // connect address and webserver together using bind
     if (bind(webserver, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
         perror("Bind error");
         return 1;
