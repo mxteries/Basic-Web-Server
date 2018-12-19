6977fb940b242733f3e8c0e5fa6866894fee61f1
Daniel Katz <dgkatz@gmail.com>
2018-12-19 15:44:28 -0500

    moved security logic to webserver

--- webserv.c
+++ webserv.c
@@ -10,7 +10,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <time.h>
-#include <linux/limits.h>
+#include <limits.h>
+#include <termios.h>
 
 /* webserv.c implements a simple HTTP 1.1 server 
 with short-lived connections using sockets 
@@ -34,6 +35,67 @@ void log_to_stdout(char* data, ssize_t data_size) {
         write(STDOUT_FILENO, data, data_size);
     }
 }
+
+int uart_open(char* path, speed_t baud){
+    struct termios uart_opts;
+    int fd = open(path, O_RDWR | O_NOCTTY | O_NDELAY);
+    // Flush data already in/out
+    if (tcflush(fd,TCIFLUSH)==-1)
+        goto err;
+    if (tcflush(fd,TCOFLUSH)==-1)
+        goto err;
+    // Setup modes (8-bit data, disable control signals, readable, no-parity)
+    uart_opts.c_cflag = CS8 | CLOCAL | CREAD;    // control modes
+    uart_opts.c_iflag=IGNPAR;                           // input modes
+    uart_opts.c_oflag=0;                                // output modes
+    uart_opts.c_lflag=0;                                // local modes
+    // Setup input buffer options: Minimum input: 1byte, no-delay
+    uart_opts.c_cc[VMIN]=1;
+    uart_opts.c_cc[VTIME]=0;
+    // Set baud rate
+    cfsetospeed(&uart_opts,baud);
+    cfsetispeed(&uart_opts,baud);
+    // Apply the settings
+    if (tcsetattr(fd,TCSANOW,&uart_opts)==-1)
+        goto err;
+    
+    return fd;
+    
+err:
+    close(fd);
+    return -1;
+}
+
+int uart_read(int argc, char** argv){
+    char buffer[1024];
+    int uart;
+    
+    if(argc < 2){
+        printf("Usage: serial-dev-path");
+        return EXIT_SUCCESS;
+    }
+    printf("Setting up\n");
+        uart = uart_open(argv[1], B57600);
+        if(uart < 0){
+            printf("Error: COuld not open %s\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+    printf("Connection Confirmed\n");
+        memset(buffer, 0, sizeof(buffer));
+        while (1){
+            // Put code here to update website!
+            read(uart, buffer, sizeof(buffer));
+            printf("%s", buffer);
+            if (strcmp(buffer, "breach") == 0) {
+                printf("Alert! Your security system has been breached!");
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+        // Connection is closed/lost, close the file and exit
+        close(uart);
+        return EXIT_SUCCESS;
+        
+    }
 
 // formats a date for the http header like: Fri, 31 Dec 1999 23:59:59 GMT
 // source: https://stackoverflow.com/a/7548846
@@ -245,20 +307,30 @@ void handle_script(int client, char* script_file, char* query_str) {
 void handle_security(int client, char* query_str) {
     handle_html(client, "security-page.html");
     if (strstr(query_str, "true") != NULL) {
-        FILE* fp;
-        char script_output[BUF_SIZE];
-        if ((fp = popen("./security", "r")) == NULL) {
-            perror("Popen script error");
-        } else {
-            // read a line from the script process and send it to the client
-            send(client, "<p>", 3, 0);
-            while (fgets(script_output, BUF_SIZE, fp) != NULL) {
-                send(client, script_output, strlen(script_output), 0);
-            }
-            pclose(fp);
-
-            send(client, "</p>", 4, 0);
+        printf("Setting up\n");
+        char* path = "/dev/cu.usbmodem14101";
+        int uart = uart_open(path, B57600);
+        if(uart < 0){
+            printf("Error: Could not open %s\n", path);
         }
+        printf("Connection Confirmed\n");
+
+        char buffer[1024];
+        memset(buffer, 0, sizeof(buffer));
+        while (1){
+            // Put code here to update website!
+            read(uart, buffer, sizeof(buffer));
+            printf("%s", buffer);
+            if (strcmp(buffer, "SECURITY BREACH") == 0) {
+                send(client, "<p>", 3, 0);
+                send(client, buffer, strlen(buffer), 0);
+                send(client, "</p>", 4, 0);
+                break;
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+        // Connection is closed/lost, close the file and exit
+        close(uart);
     }
 }
 
